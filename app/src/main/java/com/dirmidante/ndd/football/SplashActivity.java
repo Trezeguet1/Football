25af721a714b761304c944e07d450b30d0944445
Dima <dirmidante@gmail.com>
2016-12-17 13:29:32 +0300

    Added Splash Screen

---
+++ app/src/main/java/com/dirmidante/ndd/football/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.dirmidante.ndd.football;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dima on 2016-12-17.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,MainActivity.class));
+        finish();
+    }
+}
